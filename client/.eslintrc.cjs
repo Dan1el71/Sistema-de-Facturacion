0c952009608a71da2573e11cf1cd2b6739b637c3
Dan1el71 <ddrmhk1@gmail.com>
2023-12-08 17:24:43 -0500

    Added lint rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,15 +4,18 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended",
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "@typescript-eslint",],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"]
   },
 }
