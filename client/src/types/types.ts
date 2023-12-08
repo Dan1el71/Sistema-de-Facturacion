091abd569bc8d112a74be8117393a0145b2f4df9
Dan1el71 <ddrmhk1@gmail.com>
2023-12-08 17:24:54 -0500

    Added table types

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { RowData } from '@tanstack/react-table'
+
 export type State = {
   token: string
   profile: User | null
@@ -33,4 +36,32 @@ export type Client = {
   register_date: string
   social_reason: string
   state: string
+}
+
+export type Product = {
+  id: number
+  name: string
+  state: string
+  unit_price: number
+}
+
+export type Invoice = {
+  id_product: number
+  name: string
+  quantity: number
+  unitPrice: number
+  total: number
+}
+
+declare module '@tanstack/react-table' {
+  interface TableMeta<TData extends RowData> {
+    editedRows: object
+    setEditedRows: React.Dispatch<React.SetStateAction<Record<number, boolean>>>
+    updateData: (rowIndex: number, columnId: string, value: unknown) => void
+  }
+
+  interface ColumnMeta<TData extends RowData, TValue> {
+    type?: string
+    editable?: boolean | false
+  }
 }
