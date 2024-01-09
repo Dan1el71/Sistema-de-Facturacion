b34f5a3e9c997a32489be961d138698f79dc7b90
Dan1el71 <ddrmhk1@gmail.com>
2024-01-08 19:07:21 -0500

    Refactor handleApiError function to handle error messages from API responses

--- client/src/api/utils.ts
+++ client/src/api/utils.ts
@@ -1,9 +1,13 @@
 import { AxiosError } from 'axios'
 import Swal from 'sweetalert2'
 
-export const handleApiError = (err: unknown, message: string) => {
+export const handleApiError = (err: unknown, message?: string) => {
   if (err instanceof AxiosError) {
-    const errorMessage = err.response?.status === 400 ? `${message}` : 'Error'
+    const errorMessage =
+      message ||
+      (err.response?.data && Array.isArray(err.response.data)
+        ? err.response.data.map((error) => error.message).join(', ')
+        : 'Error desconocido')
 
     Swal.fire({
       title: 'Error!',
@@ -11,8 +15,6 @@ export const handleApiError = (err: unknown, message: string) => {
       icon: 'error',
       confirmButtonText: 'Aceptar',
     })
-
-    window.location.reload()
   } else {
     console.error(err)
   }
