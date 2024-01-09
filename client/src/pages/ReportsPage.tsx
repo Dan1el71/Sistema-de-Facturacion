3f08e5a81b0fcd69a21536111860c9ff944df63d
Dan1el71 <ddrmhk1@gmail.com>
2024-01-09 10:56:03 -0500

    Update ReportsPage

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -1,6 +1,12 @@
 const ReportsPage = () => {
   return (
-    <div>ReportsPage</div>
+    <div className="flex-auto overflow-y-scroll h-screen">
+      <div className="text-center m-2 ">
+        <h1 className="text-xl font-semibold p-4">Reportes</h1>
+      </div>
+
+      <h2 className="text-center">Not implemented yet</h2>
+    </div>
   )
 }
 export default ReportsPage
