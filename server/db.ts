843e2048418e4fec5fe38af39737bf7fe30afbd0
Dan1el71 <ddrmhk1@gmail.com>
2024-06-22 13:54:44 -0500

    chore: update libsql/client import in db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@libsql/client/.'
+import { createClient } from '@libsql/client'
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
